4e7d6087f73e817f089fa41616c0162309877ac3
Ross Capdeville <ross@Rosss-MacBook-Air.local>
2016-10-18 17:13:45 -0700

    adding Frequencies

---
+++ CS201/src/CountFrequncies.java
@@ -0,0 +1,48 @@
+package exercise5;
+
+/*
+    Author:
+    Date: 10/05/2016
+    Assignment: Exercise 5_2
+*/
+
+public class Exercise5_2
+{      
+    public static void main(String[] args)
+  {
+    // Initialization
+    final int ARRAY_SIZE = 20;
+    int n = ARRAY_SIZE;
+    int numbers[] = {5, 4, 6, 87, 33, 87, 5, 22, 32, 6, 87, 5, 4, 4, 18, 33, 87, 4, 6, 22};
+    int recorded[] = new int[n];
+    int recordedSize = 0;
+    int count;
+    int total=0;
+    boolean rec;
+    
+    int i = 0;
+    while(i < n) {  
+      rec = false;
+      for(int j = 0; j < recordedSize; j++) {
+        if(numbers[i] == recorded[j]) {
+          rec = true;      
+          break;
+        }
+      }
+      if(!rec && i != n-1)  {           
+        count = 0;            
+        for(int j = i; j < n; j++) {
+          if(numbers[i] == numbers[j]) {
+            count++;
+          }
+        }
+        recorded[recordedSize] = numbers[i];
+        recordedSize++;
+        System.out.println(numbers[i] + ": " + count);
+        total += count;
+      }
+      i++;
+    }
+    System.out.println("Total numbers counted: " + total);
+  }
+}
